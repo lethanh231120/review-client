c0bebdf8a3dcd6c957f72f690b34d4f661fa549b
Trieudd <fptf8fpt2018@example.com>
2023-02-02 17:25:43 +0700

    css search input at header

--- src/jsx/layouts/nav/Header.js
+++ src/jsx/layouts/nav/Header.js
@@ -66,53 +66,15 @@ const Header = ({ onNote }) => {
     })
   }, [])
 
-  // const [searchBut, setSearchBut] = useState(false);
-  var path = window.location.pathname.split('/')
-  var name = path[path.length - 1].split('-')
-  var filterName = name.length >= 3 ? name.filter((n, i) => i > 0) : name
-  var finalName = filterName.includes('app')
-    ? filterName.filter((f) => f !== 'app')
-    : filterName.includes('ui')
-      ? filterName.filter((f) => f !== 'ui')
-      : filterName.includes('uc')
-        ? filterName.filter((f) => f !== 'uc')
-        : filterName.includes('basic')
-          ? filterName.filter((f) => f !== 'basic')
-          : filterName.includes('jquery')
-            ? filterName.filter((f) => f !== 'jquery')
-            : filterName.includes('table')
-              ? filterName.filter((f) => f !== 'table')
-              : filterName.includes('page')
-                ? filterName.filter((f) => f !== 'page')
-                : filterName.includes('email')
-                  ? filterName.filter((f) => f !== 'email')
-                  : filterName.includes('ecom')
-                    ? filterName.filter((f) => f !== 'ecom')
-                    : filterName.includes('chart')
-                      ? filterName.filter((f) => f !== 'chart')
-                      : filterName.includes('editor')
-                        ? filterName.filter((f) => f !== 'editor')
-                        : filterName
   return (
     <div className={`header ${headerFix ? 'is-fixed' : ''}`}>
       <div className='header-content'>
         <nav className='navbar navbar-expand'>
-          <div className='collapse navbar-collapse justify-content-between'>
-            <div className='header-left'>
-              <div
-                className='dashboard_bar'
-                style={{ textTransform: 'capitalize' }}
-              >
-                {finalName.join(' ').length === 0
-                  ? 'Dashboard'
-                  : finalName.join(' ') === 'dashboard dark'
-                    ? 'Dashboard'
-                    : finalName.join(' ')}
-              </div>
-            </div>
-            <div className='navbar-nav header-right'>
-              <div className='nav-item d-flex align-items-center'>
-                <div className='input-group search-area '>
+          <div className='collapse navbar-collapse'>
+            {/* header: search elk input */}
+            <div className='navbar-nav header-right col-lg-12'>
+              <div className='nav-item d-flex align-items-center col-lg-12'>
+                <div className='input-group search-area full-width'>
                   <span className='input-group-text'>
                     <Link to={'#'}>
                       <svg
@@ -136,6 +98,9 @@ const Header = ({ onNote }) => {
                   />
                 </div>
               </div>
+            </div>
+            {/* side-bar right */}
+            <div className='header-right'>
               <div className='dz-side-menu'>
                 <div className='search-coundry d-flex align-items-center'>
                   <img src={United} alt='' className='mx-2' />
