b2f40ca5254e23ede23365a5c7bddbee06532ec6
Trieudd <fptf8fpt2018@example.com>
2023-02-05 18:37:25 +0700

    enhanced speed when load laund pad in soon page(detail, grid), split component list icon lunchpad/ create soon detail

---
+++ src/jsx/components/common-widgets/page-soon/LaunchpadIconList.js
@@ -0,0 +1,40 @@
+import React, { useContext } from 'react'
+import { Avatar, Tooltip } from 'antd'
+import { LaunchpadMapContext } from '../../../index'
+
+const absentData = '__'
+const LaunchpadIconList = ({ listLaunchpad }) => {
+  const launchpadMapContext = useContext(LaunchpadMapContext)
+
+  return (
+    <Avatar.Group
+      maxCount={2}
+      size={20}
+      maxStyle={{
+        color: '#fff',
+        backgroundColor: '#039F7F',
+        cursor: 'pointer'
+      }}
+    >
+      {listLaunchpad
+        ? listLaunchpad?.map((key, index) => (
+          <>
+            <Tooltip title={launchpadMapContext?.get(key)?.name}>
+              <Avatar
+                size={20}
+                src={launchpadMapContext?.get(key)?.thumbLogo}
+                key={index}
+                className='soon-table-blockchain'
+                onClick={(e) => {
+                  e.stopPropagation()
+                }}
+              />
+            </Tooltip>
+          </>
+        ))
+        : absentData}
+    </Avatar.Group>
+  )
+}
+
+export default LaunchpadIconList
