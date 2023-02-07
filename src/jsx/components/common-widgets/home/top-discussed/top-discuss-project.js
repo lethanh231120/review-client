50acc97e72a3ad1742b0c693c6a646007310461f
LongTien15 <nguyentienlong.150800@gmail.com>
2023-02-07 19:14:02 +0700

    fix hot coin, chart btc

--- src/jsx/components/common-widgets/home/top-discussed/top-discuss-project.js
+++ src/jsx/components/common-widgets/home/top-discussed/top-discuss-project.js
@@ -1,8 +1,21 @@
 import PerfectScrollbar from 'react-perfect-scrollbar'
 import { TopDiscussedItem } from './top-disussed-item'
+import { useState, useEffect } from 'react'
+import { get } from '../../../../../api/BaseRequest'
 
-export const TopDiscussed = ({ topList }) => {
-  console.log(topList)
+export const TopDiscussed = () => {
+  const [hotList, setHotList] = useState()
+
+  useEffect(() => {
+    const getHotList = async() => {
+      const res = await get('reviews/hot')
+      if (res?.code === '200') {
+        setHotList(res?.data?.products)
+      }
+    }
+    getHotList()
+  }, [])
+
   return <div className='card pb-0'>
     <div className='card-header border-0 pb-0'>
       <h2 className='heading'>Top Discussed Projects</h2>
@@ -14,7 +27,7 @@ export const TopDiscussed = ({ topList }) => {
         className='widget-media dz-scroll height370 ps ps--active-y'
       >
         <ul className='timeline'>
-          {topList?.map((item, index) => <TopDiscussedItem key={index} item={item}/>
+          {hotList?.map((item, index) => <TopDiscussedItem key={index} item={item}/>
           )}
         </ul>
       </PerfectScrollbar>
