a9d7aee004f117cd436242d88a244ad51fd3ddb7
LongTien15 <nguyentienlong.150800@gmail.com>
2023-02-15 16:00:34 +0700

    fix home, no chart detail crypto

--- src/jsx/components/common-widgets/home/reviews/review-list.js
+++ src/jsx/components/common-widgets/home/reviews/review-list.js
@@ -3,25 +3,17 @@ import { ReviewItem } from './review-item'
 
 export const ReviewList = ({ data }) => {
   let list = []
-
-  if (data?.length > 6) {
-    list = data?.slice(0, 6)
+  const NO_OF_REVIEWS_SHOW = 9
+  if (data?.length > NO_OF_REVIEWS_SHOW) {
+    list = data?.slice(0, NO_OF_REVIEWS_SHOW)
   } else {
     list = data
   }
-
   return <div className='row ' >
     <div className='col-12 mb-2'> <h2 className='heading' >Recent Reviews</h2></div>
     {!_.isEmpty(list) && list?.map((item, index) => <ReviewItem
       key={index}
-      productId={item?.productId}
-      title={item?.title}
-      star={item?.star}
-      content={item?.content}
-      username={item?.name}
-      avatar={item?.image}
-      isScam={item?.isScam}
-      productName={item?.productName}
+      data={item && item}
     />)}
   </div>
 }
