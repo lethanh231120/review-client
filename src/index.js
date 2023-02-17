8967b6ff62f2513c33a9eddb815e205209050982
LongTien15 <nguyentienlong.150800@gmail.com>
2023-02-17 11:54:05 +0700

    clean folder code

--- src/index.js
+++ src/index.js
@@ -4,20 +4,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 import { BrowserRouter } from 'react-router-dom'
-import { Provider } from 'react-redux'
-import { store } from './store/store'
 import ThemeContext from './context/ThemeContext'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <>
-    <Provider store={store}>
-      <BrowserRouter>
-        <ThemeContext>
-          <App />
-        </ThemeContext>
-      </BrowserRouter>
-    </Provider>
+    <BrowserRouter>
+      <ThemeContext>
+        <App />
+      </ThemeContext>
+    </BrowserRouter>
   </>
 )
 
--- src/jsx/index.js
+++ src/jsx/index.js
@@ -1,7 +1,6 @@
 import React, { useContext, createContext, useState } from 'react'
 // / React router dom
 import { Routes, Route, Outlet } from 'react-router-dom'
-// import { get } from '../api/BaseRequest'
 
 // / Css
 import './index.css'
@@ -18,15 +17,8 @@ import Home from './components/Dashboard/Home'
 import CategoryItem from './components/CategoryPage/CategoryItem'
 import { ConfirmEmail } from './components/auth/confirm-email'
 // product detail
-import ProductDetail1 from './components/product-detail/ProductDetail'
-import LockScreen from './pages/LockScreen'
-import Error400 from './pages/Error400'
-import Error403 from './pages/Error403'
-import Error404 from './pages/Error404'
-import Error500 from './pages/Error500'
-import Error503 from './pages/Error503'
+import ProductDetail from './components/product-detail/ProductDetail'
 import { ThemeContext } from '../context/ThemeContext'
-// import { getCookie, STORAGEKEY } from '../utils/storage'
 import ModalReport from './components/modal/modal-report/ModalReport'
 import ModalAdd from './components/modal/modal-add-product/ModalAdd'
 
@@ -34,10 +26,6 @@ import { Button, Modal } from 'react-bootstrap'
 
 export const ReportModalContext = createContext()
 export const AddModalContext = createContext()
-// export const CategoryContext = createContext()
-// export const Authenticated = createContext()
-// export const SignInContext = createContext()
-// export const LaunchpadMapContext = createContext()
 const Markup = () => {
   const [openModalReport, setOpenModalReport] = useState(false)
   const [openModalAdd, setOpenModalAdd] = useState(false)
@@ -61,12 +49,6 @@ const Markup = () => {
     <ReportModalContext.Provider value={stateReport}>
       <AddModalContext.Provider value={stateAdd}>
         <Routes>
-          <Route path='page-lock-screen' element={<LockScreen />} />
-          <Route path='page-error-400' element={<Error400 />} />
-          <Route path='page-error-403' element={<Error403 />} />
-          <Route path='page-error-404' element={<Error404 />} />
-          <Route path='page-error-500' element={<Error500 />} />
-          <Route path='page-error-503' element={<Error503 />} />
           <Route element={<MainLayout />}>
             {allroutes.map((data, i) => (
               <Route
@@ -94,24 +76,24 @@ const Markup = () => {
               <Route path='crypto'>
                 <Route path=':type'>
                   <Route path=':productName'>
-                    <Route path='' element={<ProductDetail1 />} />
-                    <Route path=':path' element={<ProductDetail1 />} />
+                    <Route path='' element={<ProductDetail />} />
+                    <Route path=':path' element={<ProductDetail />} />
                   </Route>
                 </Route>
               </Route>
               <Route path=':categoryName'>
                 <Route path=':productName'>
-                  <Route path='' element={<ProductDetail1 />} />
-                  <Route path=':path' element={<ProductDetail1 />} />
+                  <Route path='' element={<ProductDetail />} />
+                  <Route path=':path' element={<ProductDetail />} />
                 </Route>
                 <Route
                   path=':productId'
-                  element={<ProductDetail1 />}
+                  element={<ProductDetail />}
                 />
               </Route>
               <Route
                 path=':productId'
-                element={<ProductDetail1 />}
+                element={<ProductDetail />}
               />
             </Route>
           </Route>
