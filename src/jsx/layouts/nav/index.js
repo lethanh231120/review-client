15749f978683b88c92a960ac85b137d3404919a0
Trieudd <fptf8fpt2018@example.com>
2023-02-19 10:09:53 +0700

    resposive search global in header

--- src/jsx/layouts/nav/index.js
+++ src/jsx/layouts/nav/index.js
@@ -7,9 +7,13 @@ import Header from './Header'
 const JobieNav = ({ title, onClick: ClickToAddEvent, onClick2, onClick3 }) => {
   const [toggle, setToggle] = useState('')
   const onClick = (name) => setToggle(toggle === name ? '' : name)
+  const [isShowFullSearchSmallMode, setIsShowFullSearchSmallMode] = useState(false)
+
   return (
     <Fragment>
-      <NavHader />
+      <NavHader
+        isShowFullSearchSmallMode={isShowFullSearchSmallMode}
+      />
       <Header
         onNote={() => onClick('chatbox')}
         onNotification={() => onClick('notification')}
@@ -18,6 +22,8 @@ const JobieNav = ({ title, onClick: ClickToAddEvent, onClick2, onClick3 }) => {
         title={title}
         onBox={() => onClick('box')}
         onClick={() => ClickToAddEvent()}
+        isShowFullSearchSmallMode={isShowFullSearchSmallMode}
+        setIsShowFullSearchSmallMode={setIsShowFullSearchSmallMode}
       />
       <SideBar />
     </Fragment>
