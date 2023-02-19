99ea77fe6aef40f2b05542e7ded29874d082eb17
Trieudd <fptf8fpt2018@example.com>
2023-02-19 10:09:53 +0700

    resposive search global in header

--- src/jsx/layouts/nav/Header.js
+++ src/jsx/layouts/nav/Header.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect, useContext } from 'react'
 import { Dropdown } from 'react-bootstrap'
-import { Modal } from 'antd'
+import { Modal, Tooltip } from 'antd'
 
 // / Image
 import profile from '../../../images/product/user.png'
@@ -8,32 +8,44 @@ import AccountTab, {
   logInKey,
   signUpKey
 } from '../../components/common-widgets/user-form/account-tab'
-import { SignInContext, Authenticated, SignInFromAddProductContext } from '../../../App'
+import {
+  SignInContext,
+  Authenticated,
+  SignInFromAddProductContext
+} from '../../../App'
 
 import InputSearch from '../../components/input-search/GlobalSearch'
 import { getCookie, removeCookie, STORAGEKEY } from '../../../utils/storage'
 import ExpiredJWTChecker from '../../components/auth/ExpiredJWTChecker'
 import Swal from 'sweetalert2'
-import { Tooltip } from 'antd'
+// import { Tooltip } from 'antd'
 import { ReportModalContext, AddModalContext } from '../../index'
 import imgAddProject from '../../../images/svg/add-project.svg'
 import imgReportProject from '../../../images/svg/report-project-primary.svg'
 import imgLogIn from '../../../images/svg/log-in-primary.svg'
 import imgSignUp from '../../../images/svg/sign-up-primary.svg'
+import imgMiniSearch from '../../../images/svg/mini-search.svg'
+import imgCancelMiniSearch from '../../../images/svg/cancel-mini-search.svg'
+import { Link } from 'react-router-dom'
 
 const txtScamTooltip = 'Report Scam'
 const txtAddProjectTooltip = 'Add New Project'
 const txtLoginTooltip = 'Log In'
 const txtSignUpTooltip = 'Sign Up'
+const minimumWidthBigScreenMode = 767
 
-const Header = () => {
+const Header = ({ isShowFullSearchSmallMode, setIsShowFullSearchSmallMode }) => {
   // For fix header
   const [headerFix, setheaderFix] = useState(false)
+  const [isSmallMode, setIsSmallMode] = useState(window.innerWidth < minimumWidthBigScreenMode)
   const [activeTabKey, setActiveTabKey] = useState('')
 
   useEffect(() => {
     window.addEventListener('scroll', () => {
       setheaderFix(window.scrollY > 50)
+    })
+    window.addEventListener('resize', () => {
+      setIsSmallMode(window.innerWidth < minimumWidthBigScreenMode)
     })
   }, [])
 
@@ -95,6 +107,147 @@ const Header = () => {
     signInFromAddProductContext?.setIsOpenModalAddProduct(false) // reset state add product form when close login form
   }
 
+  const logedInHtml = <Dropdown
+    as='li'
+    className='nav-item dropdown header-profile'
+  >
+    <Dropdown.Toggle
+      variant=''
+      as='a'
+      className='nav-link i-false c-pointer'
+    >
+      <Tooltip title={userInfo?.userName} placement='left'>
+        <img
+          src={userInfo?.image ? userInfo?.image : profile}
+          width={20}
+          alt=''
+          style={{ backgroundColor: '#EFECE8' }} // same background of image
+        />
+      </Tooltip>
+    </Dropdown.Toggle>
+    <Dropdown.Menu
+      align='right'
+      className='dropdown-menu dropdown-menu-end'
+    >
+      <Link
+        to='#'
+        className='dropdown-item ai-icon'
+        onClick={logout}
+      >
+        <svg
+          xmlns='http://www.w3.org/2000/svg'
+          className='text-primary'
+          width='18'
+          height='18'
+          viewBox='0 0 24 24'
+          fill='none'
+          stroke='currentColor'
+          strokeWidth='2'
+          strokeLinecap='round'
+          strokeLinejoin='round'
+        >
+          <path d='M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4'></path>
+          <polyline points='16 17 21 12 16 7'></polyline>
+          <line x1='21' y1='12' x2='9' y2='12'></line>
+        </svg>
+        <span className='ms-2'>Logout</span>
+      </Link>
+    </Dropdown.Menu>
+  </Dropdown>
+
+  const loginHtml = <ul className=''>
+    <Dropdown
+      as='li'
+      className='nav-item dropdown notification_dropdown'
+    >
+      <Tooltip title={txtLoginTooltip} placement='left'>
+        <Dropdown.Toggle
+          variant=''
+          as='a'
+          className='nav-link  ai-icon i-false c-pointer button-login-home'
+          role='button'
+          onClick={() => {
+            setActiveTabKey(logInKey)
+            signContext?.handleSetOpenModal(true)
+          }}
+        >
+          <img src={imgLogIn} alt='err' />
+        </Dropdown.Toggle>
+      </Tooltip>
+    </Dropdown>
+  </ul>
+
+  const signupHtml = <ul className=''>
+    <Dropdown
+      as='li'
+      className='nav-item dropdown notification_dropdown'
+    >
+      <Tooltip title={txtSignUpTooltip} placement='left'>
+        <Dropdown.Toggle
+          variant=''
+          as='a'
+          className='nav-link  ai-icon i-false c-pointer button-signup-home'
+          role='button'
+          onClick={() => {
+            signContext?.handleSetOpenModal(true)
+            setActiveTabKey(signUpKey)
+          }}
+        >
+          <img src={imgSignUp} alt='err' />
+        </Dropdown.Toggle>
+      </Tooltip>
+    </Dropdown>
+  </ul>
+
+  const reportScamHtml = <ul className=''>
+    <Dropdown
+      as='li'
+      className='nav-item dropdown notification_dropdown'
+    >
+      <Tooltip title={txtScamTooltip} placement='left'>
+        <Dropdown.Toggle
+          variant=''
+          as='a'
+          className='nav-link  ai-icon i-false c-pointer button-report-scam-home'
+          role='button'
+          onClick={() => reportModal?.handleSetOpenModal(true)}
+        >
+          <img src={imgReportProject} alt='err' />
+        </Dropdown.Toggle>
+      </Tooltip>
+    </Dropdown>
+  </ul>
+
+  const addProjectHtml = <ul className=''>
+    <Dropdown
+      as='li'
+      className='nav-item dropdown notification_dropdown '
+    >
+      <Tooltip title={txtAddProjectTooltip} placement='left'>
+        <Dropdown.Toggle
+          variant=''
+          as='a'
+          className='nav-link  ai-icon i-false c-pointer button-add-project-home'
+          role='button'
+          onClick={handleAddProject}
+        >
+          <img src={imgAddProject} alt='err' />
+        </Dropdown.Toggle>
+      </Tooltip>
+    </Dropdown>
+  </ul>
+
+  const miniSearchHtml = <Dropdown className='sidebar-dropdown me-2 mt-2 mx-2' id='mini-search' onClick={() => setIsShowFullSearchSmallMode(!isShowFullSearchSmallMode)} style={{ display: 'flex', alignItems: 'center' }}>
+    <Dropdown.Toggle as='div' className='i-false sidebar-select'>
+      {/* image search */}
+      { isShowFullSearchSmallMode ? '' : <img src={imgMiniSearch} alt='err' /> }
+      <i className='fa-solid fa-angle-right ms-1' />
+      {/* image search */}
+      { isShowFullSearchSmallMode ? <img src={imgCancelMiniSearch} alt='err' /> : '' }
+    </Dropdown.Toggle>
+
+  </Dropdown>
+
   return (
     <>
       <Modal
@@ -107,176 +260,59 @@ const Header = () => {
       >
         <AccountTab activeTabKey={activeTabKey} />
       </Modal>
-      <div className={`header ${headerFix ? 'is-fixed' : ''}`}>
-        <div className='header-content'>
+      <div className={`header ${headerFix ? 'is-fixed' : ''} ${isShowFullSearchSmallMode ? 'p-0' : ''}`}>
+        <div className={`header-content ${isShowFullSearchSmallMode ? 'p-0' : ''}`}>
           <nav className='navbar navbar-expand'>
             <div className='collapse navbar-collapse justify-content-between'>
               {/* header: text */}
               <div className='header-left'>
                 <div
                   className='dashboard_bar text-etc-overflow'
-                  style={{ textTransform: 'capitalize', padding: '0 2rem', fontSize: '1.5rem' }}
+                  style={{
+                    textTransform: 'capitalize',
+                    padding: '0 2rem',
+                    fontSize: '1.5rem'
+                  }}
                 >
                   {`Don't trust verify`}
                 </div>
               </div>
               {/* header: search elk input */}
-              <div className='navbar-nav header-right' style={{ width: '100%' }}>
-                <div className='nav-item d-flex align-items-center' style={{ width: '100%' }}>
-                  <InputSearch />
+              <div
+                className='navbar-nav header-right'
+                // style={{ width: '100%' }}
+                style={{ width: isSmallMode ? '100%' : '60%' }}
+              >
+                <div
+                  className='nav-item d-flex align-items-center'
+                  style={{ width: '100%' }}
+                >
+                  {isSmallMode ? (isShowFullSearchSmallMode ? <InputSearch /> : '') : <InputSearch /> }
                 </div>
                 {/* side-bar right */}
                 <div className='dz-side-menu'>
-
-                  {/* Scam report, add product */}
+                  { isSmallMode ? miniSearchHtml : '' }
+                  <div className='search-coundry d-flex align-items-center'>
+                  </div>
                   <div className='sidebar-social-link '>
-                    {/* Scam report */}
                     <ul className=''>
-                      <Dropdown
-                        as='li'
-                        className='nav-item dropdown notification_dropdown'
-                        onClick={() => reportModal?.handleSetOpenModal(true)}
-                      >
-                        <Tooltip title={txtScamTooltip} placement='left'>
-                          <Dropdown.Toggle
-                            variant=''
-                            as='a'
-                            className='nav-link  ai-icon i-false c-pointer button-report-scam-home'
-                            role='button'
-                          >
-                            <img src={imgReportProject} alt='err' />
-                          </Dropdown.Toggle>
-                        </Tooltip>
-                      </Dropdown>
-                    </ul>
-                    {/* Add product */}
-                    <ul className=''>
-                      <Dropdown
-                        as='li'
-                        className='nav-item dropdown notification_dropdown '
-                        onClick={handleAddProject}
-                      >
-                        <Tooltip title={txtAddProjectTooltip} placement='left'>
-                          <Dropdown.Toggle
-                            variant=''
-                            as='a'
-                            className='nav-link  ai-icon i-false c-pointer button-add-project-home'
-                            role='button'
-                          >
-                            <img src={imgAddProject} alt='err' />
-                          </Dropdown.Toggle>
-                        </Tooltip>
-                      </Dropdown>
+                      {isShowFullSearchSmallMode ? '' : reportScamHtml}
+                      {isShowFullSearchSmallMode ? '' : addProjectHtml}
+                      {isSmallMode ? (isShowFullSearchSmallMode || authenticated?.isAuthenticated ? '' : signupHtml) : '' }
                     </ul>
                   </div>
-
-                  {/* PROFILE */}
                   <ul>
                     {authenticated?.isAuthenticated ? (
                       <>
                         <ExpiredJWTChecker logout={logout} />
-                        <Dropdown
-                          as='li'
-                          className='nav-item dropdown header-profile'
-                        >
-                          <Dropdown.Toggle
-                            variant=''
-                            as='a'
-                            className='nav-link i-false c-pointer'
-                          >
-                            <Tooltip
-                              title={userInfo?.userName}
-                              placement='left'
-                            >
-                              <img
-                                src={
-                                  userInfo?.image ? userInfo?.image : profile
-                                }
-                                width={20}
-                                alt=''
-                                style={{ backgroundColor: '#EFECE8' }} // same background of image
-                              />
-                            </Tooltip>
-                          </Dropdown.Toggle>
-                          <Dropdown.Menu
-                            align='right'
-                            className='dropdown-menu dropdown-menu-end'
-                          >
-                            {/* <LogoutPage /> */}
-                            <>
-                              <button
-                                className='dropdown-item ai-icon ms-1'
-                                onClick={logout}
-                              >
-                                <svg
-                                  xmlns='http://www.w3.org/2000/svg'
-                                  className='text-primary'
-                                  width='18'
-                                  height='18'
-                                  viewBox='0 0 24 24'
-                                  fill='none'
-                                  stroke='currentColor'
-                                  strokeWidth='2'
-                                  strokeLinecap='round'
-                                  strokeLinejoin='round'
-                                >
-                                  <path d='M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4'></path>
-                                  <polyline points='16 17 21 12 16 7'></polyline>
-                                  <line x1='21' y1='12' x2='9' y2='12'></line>
-                                </svg>
-                                <span className='ms-2'>Logout</span>
-                              </button>
-                            </>
-                          </Dropdown.Menu>
-                        </Dropdown>
+                        {isShowFullSearchSmallMode ? '' : logedInHtml}
                       </>
-                    ) : (
-                      <>
-                        <ul className=''>
-                          <Dropdown
-                            as='li'
-                            className='nav-item dropdown notification_dropdown'
-                            onClick={() => {
-                              setActiveTabKey(logInKey)
-                              signContext?.handleSetOpenModal(true)
-                            }}
-                          >
-                            <Tooltip title={txtLoginTooltip} placement='left'>
-                              <Dropdown.Toggle
-                                variant=''
-                                as='a'
-                                className='nav-link  ai-icon i-false c-pointer button-login-home'
-                                role='button'
-                              >
-                                <img src={imgLogIn} alt='err' />
-                              </Dropdown.Toggle>
-                            </Tooltip>
-                          </Dropdown>
-                        </ul>
-
-                        <ul className=''>
-                          <Dropdown
-                            as='li'
-                            className='nav-item dropdown notification_dropdown'
-                            onClick={() => {
-                              signContext?.handleSetOpenModal(true)
-                              setActiveTabKey(signUpKey)
-                            }}
-                          >
-                            <Tooltip title={txtSignUpTooltip} placement='left'>
-                              <Dropdown.Toggle
-                                variant=''
-                                as='a'
-                                className='nav-link  ai-icon i-false c-pointer button-signup-home'
-                                role='button'
-                              >
-                                <img src={imgSignUp} alt='err' />
-                              </Dropdown.Toggle>
-                            </Tooltip>
-                          </Dropdown>
-                        </ul>
+                    )
+                      : <>
+                        {isShowFullSearchSmallMode ? '' : loginHtml}
+                        {isSmallMode ? '' : signupHtml}
                       </>
-                    )}
+                    }
                   </ul>
                 </div>
               </div>
@@ -289,3 +325,4 @@ const Header = () => {
 }
 
 export default Header
+
