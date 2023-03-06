3ae4e7d5a512ee577dc62d5a2aaa343130c1672b
Trieudd <fptf8fpt2018@example.com>
2023-03-06 10:41:54 +0700

    fix

--- src/jsx/components/common-widgets/page-detail/ProductImage.js
+++ src/jsx/components/common-widgets/page-detail/ProductImage.js
@@ -2,15 +2,32 @@ import { Image } from 'antd'
 import React from 'react'
 import { formatImgUrlFromProductId, isValidProductId } from '../../../../utils/formatText'
 
-export const ProductNoImage = ({ projectName }) =>{
+export const altSoon = 'soon'
+export const altCrypto = 'crypto'
+
+const altImgSoon = 'IDO/ICO/IEO Logo'
+const altImgCrypto = 'Cryptocurrency Logo'
+const ProductNoImage = ({ projectName }) =>{
   return <span className='image-list-no-data-detail'>
     {projectName?.slice(0, 3)}
   </span>
 }
-const ProductImage = ({ productId, productName }) => {
+
+const ProductImage = ({ productId, productName, altImageType }) => {
+  let txtAlt
+  switch (altImageType) {
+    case altSoon:{
+      txtAlt = altImgSoon
+      break
+    }
+    case altCrypto:{
+      txtAlt = altImgCrypto
+      break
+    }
+  }
   return <div className='profile-photo'>
     {productId ? (
-      <Image alt='IDO/ICO/IEO Logo' src={isValidProductId(productId) ? formatImgUrlFromProductId(productId) : <ProductNoImage projectName={productName} />} preview={false}/>
+      <Image alt={txtAlt} src={isValidProductId(productId) ? formatImgUrlFromProductId(productId) : <ProductNoImage projectName={productName} />} preview={false}/>
     )
       : (
         <ProductNoImage projectName={productName} />
--- src/jsx/components/product-detail/exchange-info/ExchangeInfo.js
+++ src/jsx/components/product-detail/exchange-info/ExchangeInfo.js
@@ -53,7 +53,7 @@ const ExchangeInfo = ({ productInfo, ...rest }) => {
           <Badge className='badge-sm' >{detail?.subCategory}</Badge>
         </div>
         <div className='detail-button'>
-          <ShareButton name={detail.name} />
+          <ShareButton name={detail?.name} />
           <WebsiteButton website={detail?.website} />
         </div>
       </div>
