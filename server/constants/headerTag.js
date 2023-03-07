581173c27f661c5750112b225a75501adc82f032
Trieudd <fptf8fpt2018@example.com>
2023-03-08 06:04:36 +0700

    fix

---
+++ server/constants/headerTag.js
@@ -0,0 +1,11 @@
+
+module.exports = Object.freeze({
+  keyTitle: '__META_TITLE__',
+  keyDescription: '__META_DESCRIPTION__',
+  keyImage: '__META_IMAGE__',
+
+  defaultTitle: 'defaultTitle',
+  defaultImage: 'defaultImage',
+  defaultDescription: 'defaultDescription'
+})
+
---
+++ server/constants/route.js
@@ -0,0 +1,14 @@
+
+module.exports = Object.freeze({
+  pathProduct: 'products',
+  pathDapp: 'dapp',
+  pathVenture: 'venture',
+  pathExchange: 'exchange',
+  pathSoon: 'soon',
+  pathLaunchpad: 'launchpad',
+  pathCrypto: 'crypto',
+  pathCryptoCoin: 'coin',
+  pathCryptoToken: 'token',
+  splitDetailProduct: '_',
+  prefixDetailProduct: 'gear5'
+})
