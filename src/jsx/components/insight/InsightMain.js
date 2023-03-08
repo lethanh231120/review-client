4b36a9e8f5235a87bf0395881b5d5dac96a5e7bf
LongTien15 <nguyentienlong.150800@gmail.com>
2023-03-08 13:15:05 +0700

    add chart insight

--- src/jsx/components/insight/InsightMain.js
+++ src/jsx/components/insight/InsightMain.js
@@ -1,35 +1,57 @@
+import { useEffect, useState } from 'react'
 import { Card, Col, Row } from 'react-bootstrap'
+import { get } from '../../../api/BaseRequest'
+import { MySkeletonLoadinng } from '../common-widgets/my-spinner'
 import Barchart from './charts/BarChart'
 import LineChart from './charts/LineChart'
 import PieChart from './charts/PieChart'
 
 const InsightMain = () => {
-  return (
-    <>
+  const [loading, setLoading] = useState(true)
+  const [data, setData] = useState([])
+  console.log(data)
+  useEffect(() =>{
+    const getInsightChart = async() => {
+      const res = await get('reviews/chart')
+      if (res?.code === '200' || res?.code === 200) {
+        setData(res?.data)
+        setLoading(false)
+      }
+    }
+
+    getInsightChart()
+  }, [])
+
+  return loading ? <MySkeletonLoadinng count={4} height={100}/>
+    : <>
       <h3 style={{ marginBottom: '20px' }}>Gear5 Data Insights</h3>
       <Row >
-        {chartSection('Token Allocation Bar Chart', <Barchart/>)}
-        {chartSection('Token Allocation Pie Chart', <PieChart />)}
-        {chartSection('Blockchains Allocation Line Chart', <LineChart />)}
-        {chartSection('Token Allocation Bar Chart', <Barchart/>)}
-        {chartSection('Token Allocation Pie Chart', <PieChart />)}
-        {chartSection('Blockchains Allocation Line Chart', <LineChart />)}
-        {chartSection('Token Allocation Bar Chart', <Barchart/>)}
-        {chartSection('Token Allocation Pie Chart', <PieChart />)}
-        {chartSection('Blockchains Allocation Line Chart', <LineChart />)}
+        {data && data?.map(item => chartSection(item))}
       </Row>
     </>
-  )
 }
 
-const chartSection = (title, chart) => {
+const getChartType = (data) => {
+  switch (data?.typechart) {
+    case 'pie':
+      return <PieChart dataSet={data}/>
+    case 'line':
+      return <LineChart dataSet={data}/>
+    case 'bar':
+      return <Barchart dataSet={data}/>
+    default:
+      return null
+  }
+}
+
+const chartSection = (data) => {
   return <Col xl={4} lg={4} style={{ display: 'flex' }} >
     <Card style={{ width: '100%' }}>
       <Card.Header>
-        <h4 className='card-title'>{title}</h4>
+        <h4 className='card-title'>{data?.title}</h4>
       </Card.Header>
       <Card.Body>
-        {chart}
+        {getChartType(data)}
       </Card.Body>
     </Card>
   </Col>
