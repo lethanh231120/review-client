0cd99d8e31d2b7aa93c1716626f26280f6ed16bb
Trieudd <fptf8fpt2018@example.com>
2023-03-09 19:11:55 +0700

    fix

--- server/header-data/listDApp.js
+++ server/header-data/listDApp.js
@@ -3,22 +3,28 @@ const { getMetaTag } = require('../modal/MetaTag')
 const totalDAppProject = `12K+`
 const REPLACE_SUB_CATEGORY = '[SUB_CATEGORY]'
 
-const DESCRIPTION = `Discover Gear5's comprehensive list of ${totalDAppProject} Dapp Projects includes detailed ratings, reviews & essential information and watch out which projects are reported as Scam or Dead! `
-const DESCRIPTION_WITH_SUB_CATEGORY = `Explore the ${REPLACE_SUB_CATEGORY} and its top Dapps Projects on Gear5 with detailed ratings, reviews, and essential information and watch out which projects are reported as Scam or Dead!`
+const LIST_TITLE = `Top Dapp Projects List | Rating, Review & Details | Gear5`
+const LIST_DESCRIPTION = `Discover Gear5's comprehensive list of ${totalDAppProject} Dapp Projects includes detailed ratings, reviews & essential information and watch out which projects are reported as Scam or Dead! `
+const LIST_DESCRIPTION_OTHER = `Discover our comprehensive list of Dapp projects includes detailed ratings, reviews, and essential information and watch out which projects are reported as Scam or Dead!`
+
+const SUB_CATEGORY_TITLE = `Top ${REPLACE_SUB_CATEGORY} dApp Projects List | User Reviews, Rating & Details | Gear5 `
+const SUB_CATEGORY_DESCRIPTION = `Explore the ${REPLACE_SUB_CATEGORY} and its top Dapps Projects on Gear5 with detailed ratings, reviews, and essential information and watch out which projects are reported as Scam or Dead!
+`
+
 const staticData = {
-  title: `${REPLACE_SUB_CATEGORY} Dapp Projects List | Rating, Review & Details | Gear5`,
+  title: LIST_TITLE,
   image: `/list-dApp.webp`,
-  description: `Discover Gear5's comprehensive list of 12K+ Dapp Projects includes detailed ratings, reviews & essential information and watch out which projects are reported as Scam or Dead!`
+  description: LIST_DESCRIPTION
 }
 
 module.exports.getMetaTagListDApp = (subCategory) => getMetaTag(
   subCategory
-    ? staticData.title.replace(REPLACE_SUB_CATEGORY, `All ${subCategory}`)// Sub category
-    : staticData.title.replace(REPLACE_SUB_CATEGORY, `${totalDAppProject}`) // List
+    ? SUB_CATEGORY_TITLE?.replace(REPLACE_SUB_CATEGORY, subCategory) // SubCategory
+    : LIST_TITLE // List
   ,
   staticData.image
   ,
   subCategory
-    ? DESCRIPTION_WITH_SUB_CATEGORY.replace(REPLACE_SUB_CATEGORY, subCategory)// Sub category
-    : DESCRIPTION // List
+    ? (subCategory?.toLowerCase() === 'other') ? LIST_DESCRIPTION_OTHER : SUB_CATEGORY_DESCRIPTION?.replace(REPLACE_SUB_CATEGORY, subCategory) // SubCategory
+    : LIST_DESCRIPTION // List
 )
