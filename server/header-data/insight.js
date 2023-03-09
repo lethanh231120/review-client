36cafa47b30c049d9cffbf89d4ae03b340c99021
Trieudd <fptf8fpt2018@example.com>
2023-03-09 19:11:55 +0700

    fix

--- server/header-data/insight.js
+++ server/header-data/insight.js
@@ -1,9 +1,12 @@
 const { getMetaTag } = require('../modal/MetaTag')
 
+const LIST_TITLE = `Gear5 Data Insights in Cryptocurrency | Gear5`
+const LIST_DESCRIPTION = `A collection of charts and statistics showing market data, on-chain data, network data, top statistics, and contract data of the Ethereum, BSC, ... Blockchain.`
+
 const staticData = {
-  title: `Cryptocurrency Insights Hub | Gear5`,
+  title: LIST_TITLE,
   image: `/insight.webp`,
-  description: `Gear 5 for crypto insights with chart`
+  description: LIST_DESCRIPTION
 }
 
 module.exports.getMetaTagInsight = () => getMetaTag(staticData.title, staticData.image, staticData.description)
--- server/header-data/listCrypto.js
+++ server/header-data/listCrypto.js
@@ -1,24 +1,28 @@
 const { getMetaTag } = require('../modal/MetaTag')
-
 const totalCryptoProject = `2.3M+`
 const REPLACE_SUB_CATEGORY = '[SUB_CATEGORY]'
-const DESCRIPTION = `Discover Gear5's comprehensive list of ${totalCryptoProject} Crypto Projects includes detailed ratings, reviews & essential information and watch out which projects are reported as Scam or Dead!`
-const DESCRIPTION_WITH_SUB_CATEGORY = `Explore the ${REPLACE_SUB_CATEGORY} and its top Crypto Projects on Gear5 with detailed ratings, reviews, and essential information and watch out which projects are reported as Scam or Dead!`
+
+const LIST_TITLE = `All ${totalCryptoProject} Crypto Projects | Rating, Review & Details | Gear5`
+const LIST_DESCRIPTION = `Discover Gear5's comprehensive list of ${totalCryptoProject} Crypto Projects includes detailed ratings, reviews & essential information and watch out which projects are reported as Scam or Dead! `
+
+const SUB_CATEGORY_TITLE = `All ${REPLACE_SUB_CATEGORY} Projects | Rating, Review & Details | Gear5`
+const SUB_CATEGORY_DESCRIPTION = `Explore the ${REPLACE_SUB_CATEGORY} and its top Crypto Projects on Gear5 with detailed ratings, reviews, and essential information and watch out which projects are reported as Scam or Dead!
+`
 const staticData = {
-  title: `${REPLACE_SUB_CATEGORY} Crypto Projects | Rating, Review & Details | Gear5`,
+  title: LIST_TITLE,
   image: `/list-crypto.webp`,
-  description: DESCRIPTION
+  description: LIST_DESCRIPTION
 }
 
 module.exports.getMetaTagListCrypto = (subCategory) => getMetaTag(
   subCategory
-    ? staticData.title.replace(REPLACE_SUB_CATEGORY, `All ${subCategory}`) // Sub category
-    : staticData.title.replace(REPLACE_SUB_CATEGORY, `${totalCryptoProject}`) // List
+    ? SUB_CATEGORY_TITLE?.replace(REPLACE_SUB_CATEGORY, subCategory) // SubCategory
+    : LIST_TITLE // List
   ,
   staticData.image
   ,
   subCategory
-    ? DESCRIPTION_WITH_SUB_CATEGORY.replace(REPLACE_SUB_CATEGORY, subCategory)// Sub category
-    : DESCRIPTION // List
+    ? SUB_CATEGORY_DESCRIPTION?.replace(REPLACE_SUB_CATEGORY, subCategory) // SubCategory
+    : LIST_DESCRIPTION // List
 )
 
