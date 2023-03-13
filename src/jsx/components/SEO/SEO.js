ea1e1a81043312207d9966863c7b3a874714a896
Trieudd <fptf8fpt2018@example.com>
2023-03-13 09:56:49 +0700

    fix

--- src/jsx/components/SEO/SEO.js
+++ src/jsx/components/SEO/SEO.js
@@ -1,61 +1,12 @@
 import { Helmet } from 'react-helmet-async'
-import imgGear5Icon from '../../../images/logo/logo.png'
-import { mainColorHex } from '../../constants/color'
-import imgGear5Logo from '../../../images/logo/gear5_logo_notext.png'
 
 export const SEO = ({ props }) => {
-  const title = props?.title || `Gear5 - Don't trust, verify.`
-  const description = props?.description
-  const image = props?.image || imgGear5Logo
-
-  const baseKeywords = 'Gear5, Crypto Projects, DApps, Ventures, Exchanges, Upcomings, ICOs, IDOs, IFOs, Launchpads, Scam address, Website phishing'
-  const keywords = props?.keywords ? `${props?.keywords}, ${baseKeywords}` : baseKeywords
-  const currentHostName = window.location.hostname
-  const name = props?.name || currentHostName
-  const fullCurrentURl = window.location.href
-  const url = props?.url || fullCurrentURl
+  // <!-- Title data from index.html -->
+  const title = props?.title || `Gear 5 | 99%+ Marked As Scam or Dead /2,3M+ Crypto Projects`
 
   return (
     <Helmet>
-      {/* Standard metadata tags */}
-      <link rel='icon' href={imgGear5Icon} />
-      <link rel='canonical' href={url} />
-      {/* manifest.json provides metadata used when your web app is installed on a user's mobile device or desktop. See https://developers.google.com/web/fundamentals/web-app-manifest/ */}
-      {/* <link rel='manifest' href='%PUBLIC_URL%/manifest.json' /> */}
-      <meta charset='UTF-8' />
-      <meta name='theme-color' content={mainColorHex} />
-      {/* Setting the viewport to make your website look good on all devices: */}
-      <meta name='viewport' content='width=device-width, initial-scale=1.0' />
-
       <title>{title}</title>
-      <meta name='description' content={description} />
-      {/* Define the author of a page */}
-      <meta name='author' content={name} />
-      {/* Define keywords for search engines */}
-      <meta name='keywords' content={keywords} />
-      {/* End standard metadata tags */}
-
-      {/* Facebook tags */}
-      {/* share as post in facebook, ... */}
-      <meta property='og:title' content={title} />
-      <meta property='og:description' content={description} />
-      {/* Image display when comment on facebook, chat on slack, ... */}
-      <meta property='og:image' content={image} />
-
-      <meta property='og:type' content='website' />
-      <meta property='og:url' content={url} />
-      {/* End Facebook tags */}
-
-      {/* Twitter tags */}
-      {/* share as post in only tweeter */}
-      <meta name='twitter:title' content={title} />
-      <meta name='twitter:description' content={description} />
-      <meta name='twitter:image' content={image} />
-
-      <meta name='twitter:card' content={'summary_large_image'} />
-      <meta name='twitter:site' content={`@${url}`} />
-      <meta name='twitter:creator' content={name} />
-      {/* End Twitter tags */}
     </Helmet>
   )
 }
