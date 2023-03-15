e632fb8db2e4547dcc80cf62bf365c5a7e706c87
Trieudd <fptf8fpt2018@example.com>
2023-03-15 10:20:19 +0700

    fix

--- src/jsx/components/SEO/server/home.js
+++ src/jsx/components/SEO/server/home.js
@@ -1,3 +1,3 @@
-const LIST_TITLE = `Gear 5: 99%+ Scam/Dead Crypto Projects Among Total Of 2,3M+ | Review, Discuss & Details`
+const LIST_TITLE = `Gear 5: 99%+ Scam/Dead Crypto Projects Among Total Of 2,3M+ | Discuss, Review & Details`
 
 export const getHeaderHome = () => LIST_TITLE
