a2e497fea0cd490af633534488148079ffae18d4
Trieudd <fptf8fpt2018@example.com>
2023-03-15 16:35:39 +0700

    fix

--- src/jsx/components/SEO/server/listSoon.js
+++ src/jsx/components/SEO/server/listSoon.js
@@ -1,7 +1,7 @@
 const totalSoonProject = `45+`
 const REPLACE_SUB_CATEGORY = '[SUB_CATEGORY]'
 
-const LIST_TITLE = `All ${totalSoonProject} Projects | Rating, Discuss & Details | Gear5`
+const LIST_TITLE = `All ${totalSoonProject} ICO/IDO/IEO Projects | Rating, Discuss & Details | Gear5`
 const SUB_CATEGORY_TITLE = `All ${REPLACE_SUB_CATEGORY} Projects | Rating, Discuss & Details | Gear5`
 
 export const getHeaderListSoon = (subCategory) => subCategory
