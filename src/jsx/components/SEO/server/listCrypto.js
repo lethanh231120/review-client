52a7fc1949382f8e91017efac90cebe761d8e1a0
Trieudd <fptf8fpt2018@example.com>
2023-03-19 03:07:28 +0700

    fix

--- src/jsx/components/SEO/server/listCrypto.js
+++ src/jsx/components/SEO/server/listCrypto.js
@@ -4,8 +4,8 @@ export const totalCryptoProject = `2.3M+`
 export const SUB_TITLE = ` | Review, Discuss & Details | Gear5`
 export const REPLACE_SUB_CATEGORY = '[SUB_CATEGORY]'
 
-const LIST_TITLE = `All ${totalCryptoProject} Crypto Projects${SUB_TITLE}`
-const SUB_CATEGORY_TITLE = `All ${REPLACE_SUB_CATEGORY} Crypto Projects${SUB_TITLE}`
+const LIST_TITLE = `All ${totalCryptoProject} Cryptos${SUB_TITLE}`
+const SUB_CATEGORY_TITLE = `All ${REPLACE_SUB_CATEGORY} Cryptos${SUB_TITLE}`
 
 export const getHeaderListCrypto = (subCategory) => subCategory
   ? SUB_CATEGORY_TITLE?.replace(REPLACE_SUB_CATEGORY, toCammelCase(subCategory)) // SubCategory
