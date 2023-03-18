d86be8b1d9602bfd97e733f7369a75a17e7b1a0f
Trieudd <fptf8fpt2018@example.com>
2023-03-19 00:20:46 +0700

    fix

--- src/jsx/components/SEO/server/listExchange.js
+++ src/jsx/components/SEO/server/listExchange.js
@@ -1,4 +1,3 @@
-import { toCammelCase } from '../../../../utils/formatText'
 import { REPLACE_SUB_CATEGORY, SUB_TITLE } from './listCrypto'
 
 const totalExchange = `570+`
@@ -7,7 +6,7 @@ const totalCountDEX = `359+`
 const typeCEX = `CEX`
 const typeDEX = `DEX`
 
-const LIST_TITLE = `All ${totalExchange} Exchanges List${SUB_TITLE}`
+const LIST_TITLE = `All ${totalExchange} Crypto Exchanges${SUB_TITLE}`
 const SUB_CATEGORY_CEX_TITLE = `${totalCountCEX} ${typeCEX} Crypto Exchanges${SUB_TITLE}`
 const SUB_CATEGORY_DEX_TITLE = `${totalCountDEX} ${typeDEX} Crypto Exchanges${SUB_TITLE}`
 const SUB_CATEGORY_TITLE = `${REPLACE_SUB_CATEGORY} Crypto Exchanges${SUB_TITLE}`
@@ -16,5 +15,5 @@ export const getHeaderListExchange = (subCategory) =>subCategory
     ? SUB_CATEGORY_CEX_TITLE
     : (subCategory === typeDEX)
       ? SUB_CATEGORY_DEX_TITLE
-      : SUB_CATEGORY_TITLE?.replace(REPLACE_SUB_CATEGORY, toCammelCase(subCategory)) // SubCategory
+      : SUB_CATEGORY_TITLE?.replace(REPLACE_SUB_CATEGORY, subCategory) // SubCategory
   : LIST_TITLE // List
