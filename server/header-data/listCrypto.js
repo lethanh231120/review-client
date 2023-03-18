3ef9c84b0ee20e8e33bc6570979e0f335acfb177
Trieudd <fptf8fpt2018@example.com>
2023-03-19 03:07:28 +0700

    fix

--- server/header-data/listCrypto.js
+++ server/header-data/listCrypto.js
@@ -4,13 +4,13 @@ const totalCryptoProject = `2,3M+`
 const SUB_TITLE = ` | Review, Discuss & Details | Gear5`
 const REPLACE_SUB_CATEGORY = '[SUB_CATEGORY]'
 
-const LIST_TITLE = `All ${totalCryptoProject} Crypto Projects${SUB_TITLE}`
+const LIST_TITLE = `All ${totalCryptoProject} Cryptos${SUB_TITLE}`
 const LIST_DESCRIPTION = `Discover Gear5's comprehensive list of ${totalCryptoProject} Cryptos includes detailed ratings, reviews & essential information and watch out which projects are reported as Scam or Dead! `
 
 const IMAGE = `/list-crypto.webp`
 
-const SUB_CATEGORY_TITLE = `All ${REPLACE_SUB_CATEGORY} Crypto Projects${SUB_TITLE}`
-const SUB_CATEGORY_DESCRIPTION = `This is the list of ${REPLACE_SUB_CATEGORY}] with Real-time Price, Marketcap & Gear5's Score. Leave your Review if any of those is considered as Scam or Dead !`
+const SUB_CATEGORY_TITLE = `All ${REPLACE_SUB_CATEGORY} Cryptos${SUB_TITLE}`
+const SUB_CATEGORY_DESCRIPTION = `This is the list of ${REPLACE_SUB_CATEGORY} with Real-time Price, Marketcap & Gear5's Score. Leave your Review if any of those is considered as Scam or Dead !`
 const staticData = {
   title: LIST_TITLE,
   image: IMAGE,
