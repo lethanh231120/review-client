89b4641c98158a1da4339d514ea34fe1b379cf6e
Trieudd <fptf8fpt2018@example.com>
2023-03-19 00:20:46 +0700

    fix

--- src/jsx/components/SEO/server/listDApp.js
+++ src/jsx/components/SEO/server/listDApp.js
@@ -4,7 +4,7 @@ import { REPLACE_SUB_CATEGORY, SUB_TITLE } from './listCrypto'
 const totalDAppProject = `12K+`
 
 const LIST_TITLE = `Discuss & Details All ${totalDAppProject} DApps in the market Crypto${SUB_TITLE}`
-const SUB_CATEGORY_TITLE = `${REPLACE_SUB_CATEGORY} Dapps Projects List${SUB_TITLE}`
+const SUB_CATEGORY_TITLE = `${REPLACE_SUB_CATEGORY} DApps Projects List${SUB_TITLE}`
 
 export const getHeaderListDApp = (subCategory) => subCategory
   ? SUB_CATEGORY_TITLE?.replace(REPLACE_SUB_CATEGORY, toCammelCase(subCategory)) // SubCategory
