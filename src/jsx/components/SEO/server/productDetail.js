46b844e21e1ed354db45889d51bcbc102d9423fd
Trieudd <fptf8fpt2018@example.com>
2023-03-20 12:11:53 +0700

    fix

--- src/jsx/components/SEO/server/productDetail.js
+++ src/jsx/components/SEO/server/productDetail.js
@@ -1,7 +1,9 @@
-import { CRYPTO, DAPP, EXCHANGE, LAUNCHPAD, SOON, VENTURE } from '../../../constants/category'
+import { getStatusFromStartDateAndEndDate } from '../../../../utils/page-soon/status'
+import { CRYPTO, DAPP, EXCHANGE, LAUNCHPAD, VENTURE } from '../../../constants/category'
 import { getHeaderHome } from './home'
 import { SUB_TITLE } from './listCrypto'
 
+const META_TITLE = getHeaderHome()
 const isInteger = (number) => {
   return (typeof number === 'number') && Math.floor(number) === number
 }
@@ -47,7 +49,7 @@ const getTxtAdditional = (isScam, isWarning, score, type) =>{
 
 export const getHeaderProductDetail = (productDetail) => {
   const data = productDetail
-  let title = data?.name || data?.ventureName || data?.dAppName || data?.projectName || getHeaderHome()
+  let title = data?.name || data?.ventureName || data?.dAppName || data?.projectName || META_TITLE
   const productId = data?.cryptoId || data?.dAppId || data?.ventureId || data?.exchangeId || data?.projectId || data?.launchPadId
 
   let totalInteract = ''
@@ -81,28 +83,52 @@ export const getHeaderProductDetail = (productDetail) => {
   const rawScore = data?.score
   switch (productId) {
     case data?.cryptoId :{
-      title = `${getTxtAdditional(isScam, isWarning, rawScore, CRYPTO)}${title}${data?.symbol ? ` (${data?.symbol})` : ''},${totalInteract} Crypto Projects${extraData}`
+      if (data?.type === 'token') {
+        title = `${data?.name} ${data?.symbol} ${data?.address} review on Gear5`
+      } else if (data?.type === 'coin') {
+        if ((isInteger(totalScam) && totalScam > 0) || (isInteger(totalReview) && totalReview > 0)) {
+          title = `Review & Discuss ${data?.name} ${data?.symbol} -${totalScam ? ` ${totalScam} reported as a scam` : ''}${totalReview ? ` ${totalReview} comment` : ''} | Gear5`
+        } else {
+          title = `${data?.name} ${data?.symbol} - Review & Discuss Cryptocurrency projects | Gear5`
+        }
+      } else {
+        title = `${getTxtAdditional(isScam, isWarning, rawScore, CRYPTO)}${title}${data?.symbol ? ` (${data?.symbol})` : ''},${totalInteract} Crypto Projects${extraData}`
+      }
       break
     }
     case data?.dAppId :{
-      title = `${getTxtAdditional(isScam, isWarning, rawScore, DAPP)}${title},${totalInteract} Decentralized Application${extraData}`
+      if ((isInteger(totalScam) && totalScam > 0) || (isInteger(totalReview) && totalReview > 0)) {
+        title = `Review ${data?.dAppName} project -${totalScam ? ` ${totalScam} reported as a scam` : ''}${totalReview ? ` ${totalReview} comment` : ''} | Gear5`
+      } else {
+        title = `Reviews & Discuss ${data?.dAppName} - project Dapps | Gear5`
+      }
       break
     }
     case data?.ventureId :{
-      title = `${getTxtAdditional(isScam, isWarning, rawScore, VENTURE)}${title},${totalInteract} Crypto Ventures${extraData}`
+      title = `${data?.ventureName} Crypto Ventures | Portfolio & Details | Gear5`
       break
     }
     case data?.exchangeId :{
-      title = `${getTxtAdditional(isScam, isWarning, rawScore, EXCHANGE)}${title},${totalInteract} Crypto Exchanges${extraData}`
+      if ((isInteger(totalScam) && totalScam > 0) || (isInteger(totalReview) && totalReview > 0)) {
+        title = `${data?.name} ${data?.subCategory} -${totalScam ? ` ${totalScam} reported as a scam` : ''}${totalReview ? ` ${totalReview} comment` : ''} | Crypto Exchanges | Gear5`
+      } else {
+        title = `${data?.name} ${data?.subCategory} | Reviews & Discuss Crypto Exchanges | Gear5`
+      }
       break
     }
     // Soon Project
     case data?.projectId :{
-      title = `${getTxtAdditional(isScam, isWarning, rawScore, SOON)}${title}${data?.projectSymbol ? ` (${data?.projectSymbol})` : ''},${totalInteract} ICO/IDO/IEO Projects${extraData}`
+      let soonStatus = getStatusFromStartDateAndEndDate(data?.startDate, data?.endDate)
+      if (isInteger(totalReview) && totalReview > 0) {
+        soonStatus = soonStatus ? `${soonStatus} |` : soonStatus
+        title = `${data?.projectName} ${data?.projectSymbol} - ${data?.roundType} - ${totalReview} Reviews |${soonStatus} Gear5`
+      } else {
+        title = `${data?.projectName} ${data?.projectSymbol} - ${data?.roundType}${soonStatus} | Project details | Gear5`
+      }
       break
     }
     case data?.launchPadId :{
-      title = `${getTxtAdditional(isScam, isWarning, rawScore, LAUNCHPAD)}${title},${totalInteract} Crypto Launchpads${extraData}`
+      title = `${data?.name} Crypto Launchpads | Invest Data & Details | Gear5`
       break
     }
   }
