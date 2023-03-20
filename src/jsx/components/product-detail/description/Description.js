93225470fd7f5cdc016acd2ecf9f4a0064bbd1b7
Trieudd <fptf8fpt2018@example.com>
2023-03-20 19:44:10 +0700

    fix

--- src/jsx/components/product-detail/description/Description.js
+++ src/jsx/components/product-detail/description/Description.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import './description.scss'
 import { toCammelCase } from '../../../../utils/formatText'
+import _ from 'lodash'
 
 // projectName has data will description detail
 // projectName has no data --> review
-const Description = ({ projectName, text, descriptionToken, chainName }) => {
+const Description = ({ projectName, text, descriptionToken, descriptionTokenAddress, chainName, descriptionTokenMultichain }) => {
   // max length displayed first time(more length withh display show more, less length withh display all)
   const length = 500
   const [showLess, setShowLess] = React.useState(true)
@@ -25,22 +26,16 @@ const Description = ({ projectName, text, descriptionToken, chainName }) => {
           <div className='description-list'>
             <div className='card-content' style={{ fontSize: '1rem', lineHeight: '2' }}>
               {text?.length < length ? (
-                <p className='cus-text-justify' dangerouslySetInnerHTML={{ __html: `${text}` }}></p>
+                <span className='cus-text-justify' dangerouslySetInnerHTML={{ __html: `${text}` }}></span>
               ) : (
                 <>
-                  {descriptionToken
-                    ? <>
-                      <h1 style={{ fontSize: '14px', color: '#A098AE', fontWeight: '400', display: 'inline' }}>{descriptionToken}</h1>
-                      on chain {toCammelCase(chainName)}.&nbsp;
-                    </>
-                    : ''}
-                  <p
+                  <span
                     style={{ display: 'inline' }}
                     className='cus-text-justify'
                     dangerouslySetInnerHTML={{
                       __html: showLess ? `${text?.slice(0, 350)}...` : `${text}`
                     }}
-                  ></p>
+                  ></span>
                   <span
                     style={{ color: '#18A594' }}
                     className='read-or-hide'
@@ -50,6 +45,34 @@ const Description = ({ projectName, text, descriptionToken, chainName }) => {
                   </span>
                 </>
               )}
+              {descriptionToken && descriptionTokenAddress
+                ? <>
+                  <h2 style={{ fontSize: '14px', color: '#A098AE', fontWeight: '400', display: 'inline' }}>{descriptionToken}</h2> {`(`}
+                  <h1 style={{ fontSize: '14px', color: '#A098AE', fontWeight: '400', display: 'inline' }} className='text-primary'>{descriptionTokenAddress}</h1>{`)`}
+                  &nbsp;on chain {toCammelCase(chainName)}.&nbsp;
+                </>
+                : ''}
+              { !_.isEmpty(descriptionTokenMultichain)
+                ? <>
+                  <br />
+                  <b className='fs-22'>Chain(s):</b>
+                  <br />
+
+                  <div>
+                    {descriptionTokenMultichain.map((chain) => (<>
+                      {
+                        chain?.split('_')?.length >= 4
+                          ? <div>
+                            {toCammelCase(chain?.split('_')[2])}: <h3 style={{ fontSize: '14px', color: '#A098AE', fontWeight: '400', display: 'inline' }} className='text-primary'>{chain?.split('_')[3]}</h3>
+                          </div>
+                          : ''
+                      }
+                    </>
+                    ))}
+                  </div>
+                  &nbsp;
+                </>
+                : ''}
             </div>
           </div>
         </div>
