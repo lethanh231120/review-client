23ef4ddde9a419c74c477a4c39a120f7d469be4b
Trieudd <fptf8fpt2018@example.com>
2023-03-21 16:11:08 +0700

    fix

--- server/index.js
+++ server/index.js
@@ -68,25 +68,25 @@ const isInteger = (number) => {
 
 const injectHtmlHeader = (metaTag) => {
   let dynamicMetaIndexHtml = file?.getIndexHtml()
-  // replace image first(contain main url)
+
+  // replace image twitter first(contain main url)
   const isInternalImage = metaTag?.image && metaTag?.image?.length >= 1 && metaTag?.image[0] === '/'
+  let twitterUrl = ''
   if (isInternalImage) {
-    dynamicMetaIndexHtml = dynamicMetaIndexHtml
-      ?.split(META_UNIQUE_LINK + META_IMAGE)?.join(META_UNIQUE_LINK + metaTag?.image)
+    twitterUrl = META_UNIQUE_LINK + metaTag?.image
   } else {
     // External image
-    dynamicMetaIndexHtml = dynamicMetaIndexHtml
-      ?.split(META_UNIQUE_LINK + META_IMAGE)?.join(metaTag?.image)
+    twitterUrl = metaTag?.image
   }
   dynamicMetaIndexHtml = dynamicMetaIndexHtml
-    ?.split(META_UNIQUE_LINK)?.join(metaTag?.uniqueLink)
+    ?.split(`<meta name="twitter:image" content="https://gear5.io/logo.webp"/>`)?.join(`<meta name="twitter:image" content="${twitterUrl}"/>`)
 
   dynamicMetaIndexHtml = dynamicMetaIndexHtml
     ?.split(META_TITLE)?.join(metaTag?.title) // euqal replace all
     ?.split(META_DESCRIPTION)?.join(metaTag?.description)
+    ?.split(META_IMAGE)?.join(metaTag?.image)
+    ?.split(META_UNIQUE_LINK)?.join(metaTag?.uniqueLink)
 
-  // const schemaMarkupIndexHtml = dynamicMetaIndexHtml?.replace(getScriptSchemaMarkupSiteLinkSearchBoxHomePage(), '')
-  // return schemaMarkupIndexHtml
   return dynamicMetaIndexHtml
 }
 
