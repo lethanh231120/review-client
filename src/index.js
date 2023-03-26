fa69f065b63c0c159e5c2b11b626ae1893294cb5
Trieudd <fptf8fpt2018@example.com>
2023-03-27 05:09:42 +0700

    add try catch for new update version

--- src/index.js
+++ src/index.js
@@ -5,16 +5,43 @@ import reportWebVitals from './reportWebVitals'
 import { BrowserRouter } from 'react-router-dom'
 import ThemeContext from './context/ThemeContext'
 import { HelmetProvider } from 'react-helmet-async'
+import { ErrorBoundary } from 'react-error-boundary'
+import { emailNika } from './jsx/components/privacy-policy/PrivacyPolicy'
 
+const templateNewVersion = <>
+  <p style={{
+    fontSize: '4rem',
+    color: '#039F7F' // main color
+  }}>
+    <center>The website has a new update.</center>
+  </p>
+
+  <p style={{
+    fontSize: '2rem',
+    color: '#039F7F' // main color
+  }}>
+  Please clear your browser cache, or press key combination <code>Ctrl</code> <code>Shift</code> <code>R</code>
+  </p>
+
+  <p style={{
+    fontSize: '2rem',
+    color: '#039F7F' // main color
+  }}>
+  In the worst case try closing the browser and reopening it, or contact us via email <b>{emailNika}</b>
+  </p>
+
+</>
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
-  <HelmetProvider>
-    <BrowserRouter>
-      <ThemeContext>
-        <App />
-      </ThemeContext>
-    </BrowserRouter>
-  </HelmetProvider>,
+  <ErrorBoundary fallback={templateNewVersion}>
+    <HelmetProvider>
+      <BrowserRouter>
+        <ThemeContext>
+          <App />
+        </ThemeContext>
+      </BrowserRouter>
+    </HelmetProvider>
+  </ErrorBoundary>,
   document.getElementById('root')
 
 )
