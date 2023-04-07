b6983ded01a6618996b9e36c2dc38cf75ca94dde
Trieudd <fptf8fpt2018@example.com>
2023-04-07 17:33:00 +0700

    fix

--- src/jsx/components/referral-code/ReferralWithdrawHistory.js
+++ src/jsx/components/referral-code/ReferralWithdrawHistory.js
@@ -118,7 +118,7 @@ export const ReferralWithdrawHistory = ({ newClaimedHistory }) => {
     <Card style={{ height: '100%' }}>
       <Card.Header>
         <Card.Title>
-          <h3 className='heading text-center' style={{ textTransform: 'none' }}>{`Widthdrawal History`}</h3>
+          <h3 className='heading text-center text-primary' style={{ textTransform: 'none' }}>{`Widthdrawal History`}</h3>
         </Card.Title>
       </Card.Header>
       <Card.Body>
