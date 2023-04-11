da2e7fa1b0adffb58eb716c3d25113ebaa7b5a73
Trieudd <fptf8fpt2018@example.com>
2023-04-11 12:02:48 +0700

    fix

--- server/index.js
+++ server/index.js
@@ -31,6 +31,15 @@ const file = (function() {
 
 const axios = require('axios')
 const isbot = require('isbot')
+const rateLimit = require('express-rate-limit')
+const limiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minutes
+  max: 60, // Limit each IP to 60 requests per `window` (here, per 1 minutes)
+  standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+  legacyHeaders: false // Disable the `X-RateLimit-*` headers
+})
+// Apply the rate limiting middleware to all requests
+app.use(limiter)
 
 const { getMetaTagHome } = require('./header-data/home')
 const { getMetaTagListCrypto, getSubTitle } = require('./header-data/listCrypto')
